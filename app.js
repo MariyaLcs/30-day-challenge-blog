99ceeb6b2b73bd4b78854e119852401c1891911b
Mariya <mariya.podosinova@gmail.com>
2020-08-03 12:59:09 +0100

    passing a variable to home page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render(__dirname + "/views/home.ejs");
+  res.render("home", { startingPost: homeStartingContent });
 });
 
 app.listen(3000, function () {
