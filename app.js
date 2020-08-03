12690f050e04bbf2677ef9fa35e93c1082fab572
Mariya <mariya.podosinova@gmail.com>
2020-08-03 13:30:04 +0100

    about and mor epages

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const ejs = require("ejs");
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  "The upcoming month is all about focusing on self-care and finding ways to make physical and mental health a bigger part of your life, which may sound like a lot but in practice is pretty simple. We've designated one easy task per day, so you'll never feel too overwhelmed.";
+const moreContent =
+  "Don't think it's selfish: 'When we've taken good care of ourselves, not only do we have more energy for others, but we we tend to be more focused and more present,' Dr. Coons says. Tap a friend: When we share our goals, we do better. Get a group of two, three, or four friends for added accountability. If you miss a day, don’t give up: The goal isn't to be perfect. Even if you just do 25 or 15 days, that's still an improvement from the previous month. Keep it up afterward: Improving your wellbeing is an ongoing process, so adopt one or two new habits that changed your mood for the better.";
 
 const app = express();
 
@@ -20,6 +20,14 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("home", { startingPost: homeStartingContent });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutPost: aboutContent });
+});
+
+app.get("/more", function (req, res) {
+  res.render("more", { morePost: moreContent });
 });
 
 app.listen(3000, function () {
